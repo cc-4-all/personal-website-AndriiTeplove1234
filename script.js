01af0ffb4e261872ca8ee499efecabb531009ed4
AndriiTeplove1234 <136475823+AndriiTeplove1234@users.noreply.github.com>
2023-06-14 16:29:21 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-var img = document.getELementById("gallery-img");
-var prev = document.getELementById("prev");
-var next = document.getELementById("next");
+var img = document.getElementById("gallery-img");
+var prev = document.getElementById("prev");
+var next = document.getElementById("next");
 
-var images = ["image_2.jpg"];
+var images = ['anime.jpg', 'cat.jpg', 'image_2.jpg', 'image_3.jpg', 'image_4.jpg', 'image_5.jpg', 'image_6.jpg', 'selfie.jpg'];
+
+var currentimage = 0;
 
 function changeimage(e) {
-if (e.target.id == "next" && currentimage !== imagens.length - 1) {
+if (e.target.id == "next" && currentimage !== images.length - 1) {
     currentimage = currentimage + 1;
 }
 
@@ -13,8 +15,8 @@ if (e.target.id == "prev" && currentimage !==0) {
     currentimage = currentimage - 1;
 }
 
-img.setAttrlistener("src", "images/" + images[currentimage]);
+img.setAttribute("src", "images/" + images[currentimage]);
 }
 
-prev.addevntlister("click", changeimage);
-next.addevntlister("chick", changeimage);
+prev.addEventListener("click", changeimage);
+next.addEventListener("click", changeimage);
